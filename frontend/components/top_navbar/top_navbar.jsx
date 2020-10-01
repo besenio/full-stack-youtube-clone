ce1c42e28e92ed070f8b66376ceb566875f483ed
besenio <kbesenio2@yahoo.com>
2020-09-30 18:48:25 -0700

    sidebar complete

--- frontend/components/top_navbar/top_navbar.jsx
+++ frontend/components/top_navbar/top_navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { openModal } from '../../actions/modal_actions';
 
 class TopNavbar extends React.Component {
     constructor(props) {
