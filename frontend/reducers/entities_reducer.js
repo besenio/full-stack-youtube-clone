8cec99f47c14fa84f8102ada43b13bdd231a5c9e
besenio <kbesenio2@yahoo.com>
2020-10-09 01:37:10 -0700

    entities reducer updated

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
 import usersReducer from "./users_reducer";
 import videosReducer from "./videos_reducer";
-import CommentsReducer from "./comments_reducer"
+import commentsReducer from "./comments_reducer"
+import likesReducer from "./likes_reducer";
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   videos: videosReducer,
-  comments: CommentsReducer
+  comments: commentsReducer,
+  like: likesReducer
 });
 
 export default entitiesReducer;
