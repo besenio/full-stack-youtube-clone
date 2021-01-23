75dc93891f2a9730eb8829b8a5e99bec728b6e12
besenio <kbesenio2@yahoo.com>
2021-01-22 20:13:42 -0800

    implemented useEffect hook to fetch videos

--- frontend/components/videos/video_index_side.jsx
+++ frontend/components/videos/video_index_side.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
-import { Redirect, Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
 import VideoIndexItem from './video_index_item';
 
-class VideoIndexSide extends React.Component {
-    componentDidMount() {
-        this.props.fetchVideos();
-    }
+const videoIndexSide = (props) => {
+    useEffect(() => {
+        props.fetchVideos();
+    }, []);
 
-    render() {
-        return (
-            <div className="videos-index">
-                <div className="video-index-main">
-                    <h1>Up Next</h1>
-                    <ul className="videos-all">
-                        {
-                            this.props.videos.map(video => (
-                                <VideoIndexItem video={video} key={video.id}/>
-                            ))
-                        }
-                    </ul>
-                </div>
+    return (
+        <div className="videos-index">
+            <div className="video-index-main">
+                <h1>Up Next</h1>
+                <ul className="videos-all">
+                    {
+                        props.videos.map(video => (
+                            <VideoIndexItem video={video} key={video.id}/>
+                        ))
+                    }
+                </ul>
             </div>
-        )
-    }
+        </div>
+    );
+};
 
-}
-
-export default VideoIndexSide;
+export default videoIndexSide;
