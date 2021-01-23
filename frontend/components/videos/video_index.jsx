526668783492a5a89519e38345a0ceb1090ae2ec
besenio <kbesenio2@yahoo.com>
2021-01-22 19:59:15 -0800

    implemented useEffect hook to fetch videos

--- frontend/components/videos/video_index.jsx
+++ frontend/components/videos/video_index.jsx
@@ -1,47 +1,44 @@
-import React from 'react';
-import { Redirect, Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import VideoIndexItem from './video_index_item';
 
-class VideoIndex extends React.Component {
-    componentDidMount() {
-        this.props.fetchVideos();
-    }
+const videoIndex = (props) => {
+    useEffect(() => {
+        props.fetchVideos();
+    }, []);
 
-    render() {
-        return (
-            <div className="videos-index">
-                <div className="video-index-sidebar">
-                    <Link to='/' className="video-index-home">
-                        <i className="fas fa-home"></i>
-                        <div>Home</div>
-                    </Link>
-                    <a href="https://github.com/besenio" target="_" className="video-index-github">
-                        <i className="fab fa-github"></i>
-                        <div>Github</div>
-                    </a>
-                    <a href="https://www.linkedin.com/in/kevin-besenio/" target="_" className="video-index-linkedin">
-                        <i className="fab fa-linkedin"></i>
-                        <div>LinkedIn</div>
-                    </a>
-                    <a href="https://angel.co/u/besenio" target="_" className="video-index-angellist">
-                        <i className="fab fa-angellist"></i>
-                        <div>AngelList</div>
-                    </a>
-                </div>
-                <div className="video-index-main">
-                    <h1>Recommended</h1>
-                    <ul className="videos-all">
-                        {
-                            this.props.videos.map(video => (
-                                <VideoIndexItem video={video} key={video.id}/>
-                            ))
-                        }
-                    </ul>
-                </div>
+    return (
+        <div className="videos-index">
+            <div className="video-index-sidebar">
+                <Link to='/' className="video-index-home">
+                    <i className="fas fa-home"></i>
+                    <div>Home</div>
+                </Link>
+                <a href="https://github.com/besenio" target="_" className="video-index-github">
+                    <i className="fab fa-github"></i>
+                    <div>Github</div>
+                </a>
+                <a href="https://www.linkedin.com/in/kevin-besenio/" target="_" className="video-index-linkedin">
+                    <i className="fab fa-linkedin"></i>
+                    <div>LinkedIn</div>
+                </a>
+                <a href="https://angel.co/u/besenio" target="_" className="video-index-angellist">
+                    <i className="fab fa-angellist"></i>
+                    <div>AngelList</div>
+                </a>
             </div>
-        )
-    }
+            <div className="video-index-main">
+                <h1>Recommended</h1>
+                <ul className="videos-all">
+                    {
+                        props.videos.map(video => (
+                            <VideoIndexItem video={video} key={video.id}/>
+                        ))
+                    }
+                </ul>
+            </div>
+        </div>
+    );
+};
 
-}
-
-export default VideoIndex;
+export default videoIndex;
