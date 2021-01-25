3b84acfb831f347a0450e016fac6021ce63a094a
besenio <kbesenio2@yahoo.com>
2021-01-24 22:13:53 -0800

    implement react hooks to navigation bar component

--- frontend/components/top_navbar/top_navbar.jsx
+++ frontend/components/top_navbar/top_navbar.jsx
@@ -1,51 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProfileDropdown from './profile_dropdown';
 
-class TopNavbar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showDropdown: false,
-            search: ""
+const topNavbar = (props) => {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [search, setSearch] = useState('');
+
+    const handleInput = (e) => {
+        setSearch(e.currentTarget.value);
+    }
+
+    const handleSearch = () => {
+        if (search !== '') {
+            props.history.push(`/search/${search}`);
         }
-        this.submit = this.submit.bind(this);
-        this.handleDropdown = this.handleDropdown.bind(this);
-        this.handleInput = this.handleInput.bind(this);
-        this.handleSearch = this.handleSearch.bind(this);
+        setSearch('');
     }
 
-    handleInput(e) {
-        this.setState({ search: e.currentTarget.value });
+    const submit = () => {
+        props.openSidebar(true);
     }
 
-    handleSearch() {
-        if (this.state.search !== "") {
-            this.props.history.push(`/search/${this.state.search}`);
-        }
-        this.setState({ search: "" });
+    const handleDropdown = () => {
+        setShowDropdown(!showDropdown);
     }
 
-    submit() {
-        this.props.openSidebar(true);
-    }
-
-    handleDropdown() {
-        this.setState({ showDropdown: !this.state.showDropdown });
-    }
-
-    display() {
-        return (this.props.currentUser ? (
+    const display = () => {
+        return (props.currentUser ? (
         <div className="signed-in-greeting">
-            <div className="dropdown-greeting" onClick={this.handleDropdown}>
-                {this.props.currentUser.username[0].toUpperCase()}
+            <div className="dropdown-greeting" onClick={handleDropdown}>
+                {props.currentUser.username[0].toUpperCase()}
             </div>
             <div className="drop-down-menu">
                 <ProfileDropdown
-                    logout={this.props.logout}
-                    currentUser={this.props.currentUser}
-                    handleDropdown={this.handleDropdown}
-                    showDropdown={this.state.showDropdown}
+                    logout={props.logout}
+                    currentUser={props.currentUser}
+                    handleDropdown={handleDropdown}
+                    showDropdown={showDropdown}
                 />
             </div>
         </div>) : (
@@ -57,49 +48,45 @@ class TopNavbar extends React.Component {
         </div>)
     )}
 
-
-
-    render() {
-        return (
-            <div className="top-navbar">
-                <div className="top-navbar-left">
-                    <div className="top-navbar-sidebar">
-                        <i className="fas fa-bars" onClick={this.submit}></i>
-                    </div>
-                    <Link to="/" className="top-navbar-splash">
-                        <i className="fab fa-youtube"></i>
-                        <div>ViewTube</div>
-                    </Link>
+    return (
+        <div className="top-navbar">
+            <div className="top-navbar-left">
+                <div className="top-navbar-sidebar">
+                    <i className="fas fa-bars" onClick={submit}></i>
                 </div>
-                <div className="top-navbar-center">
-                    <form className="navbar-center-bar" onSubmit={this.handleSearch}>
-                        <input type="text" placeholder="Search" onChange={this.handleInput} value={this.state.search}/>
-                    </form>
-                    <button className="navbar-center-button" onClick={this.handleSearch}>
-                        <i className="fas fa-search"></i>
-                    </button>
+                <Link to="/" className="top-navbar-splash">
+                    <i className="fab fa-youtube"></i>
+                    <div>ViewTube</div>
+                </Link>
+            </div>
+            <div className="top-navbar-center">
+                <form className="navbar-center-bar" onSubmit={handleSearch}>
+                    <input type="text" placeholder="Search" onChange={handleInput} value={search}/>
+                </form>
+                <button className="navbar-center-button" onClick={handleSearch}>
+                    <i className="fas fa-search"></i>
+                </button>
+            </div>
+            <div className="top-navbar-right">
+                <div className="top-navbar-video">
+                    <i className="fas fa-video "
+                        onClick={() => {
+                            (props.currentUser ? props.openModal({type: 'createVideo'}) : props.history.push('/login'))
+                        }}
+                    ></i>
                 </div>
-                <div className="top-navbar-right">
-                    <div className="top-navbar-video">
-                        <i className="fas fa-video "
-                            onClick={() => {
-                                    (this.props.currentUser ? this.props.openModal({type: 'createVideo'}) : this.props.history.push('/login'))
-                            }}
-                        ></i>
-                    </div>
-                    <div className="top-navbar-apps">
-                        <i className="fas fa-th"></i>
-                    </div>
-                    <div className="top-navbar-settings">
-                        <i className="fas fa-ellipsis-v"></i>
-                    </div>
-                    <div className="top-navbar-profile">
-                        <div>{this.display()}</div>
-                    </div>
+                <div className="top-navbar-apps">
+                    <i className="fas fa-th"></i>
+                </div>
+                <div className="top-navbar-settings">
+                    <i className="fas fa-ellipsis-v"></i>
+                </div>
+                <div className="top-navbar-profile">
+                    <div>{display()}</div>
                 </div>
             </div>
-        )
-    }
-}
+        </div>
+    );
+};
 
-export default TopNavbar;
+export default topNavbar;
