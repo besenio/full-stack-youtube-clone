41abae0281a002975222399a5c91416380816bda
besenio <kbesenio2@yahoo.com>
2021-01-28 17:36:52 -0800

    google analytics

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import TopNavbarContainer from "./top_navbar/top_navbar_container";
 import SideNavbarContainer from "./side_navbar/side_navbar_container";
@@ -7,19 +7,27 @@ import Modal from "./modal/modal_conatiner"
 import VideoShowContainer from './videos/video_show_container';
 import EditVideoFormContainer from "./videos/edit_video_form_container";
 import SearchVideoIndexContainer from "./search/search_video_index_container";
+import ReactGA from 'react-ga';
 
-const App = () => (
-    <div>
-        <Route path="/" component={TopNavbarContainer}/>
-        <SideNavbarContainer />
-        <Modal />
-        <Route exact path="/" component={VideoIndexContainer}/>
-        <Switch>
-            <Route exact path="/edit/:videoId" component={EditVideoFormContainer}/>
-            <Route exact path="/videos/:videoId" component={VideoShowContainer} />
-            <Route exact path='/search/:query' component={SearchVideoIndexContainer} />
-        </Switch>
-    </div>
-);
+const App = () => {
+    useEffect(() => {
+        ReactGA.initialize('G-YKYXGQZ7E6');
+        ReactGA.pageview(window.location.pathname);
+    }, []);
+
+    return (
+        <div>
+            <Route path="/" component={TopNavbarContainer}/>
+            <SideNavbarContainer />
+            <Modal />
+            <Route exact path="/" component={VideoIndexContainer}/>
+            <Switch>
+                <Route exact path="/edit/:videoId" component={EditVideoFormContainer}/>
+                <Route exact path="/videos/:videoId" component={VideoShowContainer} />
+                <Route exact path='/search/:query' component={SearchVideoIndexContainer} />
+            </Switch>
+        </div>
+    )
+};
 
 export default App;
